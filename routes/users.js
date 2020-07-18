a1ff81e2f4a4b81a99ffcaa43f2955c10fa1c517
Dawood Muzammil <dawood.malik@bordatech.com>
2020-07-19 01:07:36 +0300

    Set up skeleton for user routes

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('GET /users');
 });
 
+/* GET user information */
+router.get('/:id', function(req, res, next) {
+  res.send('GET /users/:id');
+});
+
+/* POST create a new user */
+router.post('/', function(req, res, next) {
+  res.send('POST /users');
+});
 module.exports = router;
